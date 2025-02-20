220c9cfb1aaaa49741c6dccd9ce963cdcaf9b649
HibHaqqi <haqqi09@gmail.com>
2025-02-20 15:55:14 +0800

    edit for total unit

--- app/config.py
+++ app/config.py
@@ -5,5 +5,5 @@ load_dotenv()
 class Config:
     """Base configuration."""
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_default_secret_key'
-    SQLALCHEMY_DATABASE_URI =  'postgresql://postgres:rahasia@localhost:5432/hmrecord'
+    SQLALCHEMY_DATABASE_URI =  os.environ.get('DATABASE_URL') 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
